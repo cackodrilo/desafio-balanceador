8581cb95355baeb48c0968b5a3b1bce5cd837e93
CARLOS <CMATURANA.QUESILLE@GMAIL.COM>
2021-11-30 17:38:14 -0300

    App para evaluar textos

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,27 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def balanceador
+    result = 0
+    cadena = params[:search]
+    count = 0
+    cadena.each_char.with_index do |val, key|
+        if val == "("
+            count += 1
+        elsif val == ')'
+            count -= 1
+            if count < 0
+              break
+            end
+        end
+    end
+
+    if count == 0
+      mensaje = 'Balanceado'
+    else
+      mensaje = 'Desbalanceado'
+    end
+
+    redirect_to root_path, alert: mensaje
+  end
+end
