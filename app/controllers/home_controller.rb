26e2fac8786c5b2ae1cf768fc89f9de86d63abfd
CARLOS <CMATURANA.QUESILLE@GMAIL.COM>
2021-12-01 17:16:22 -0300

    Evaluacion de reglas

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,31 @@
 class HomeController < ApplicationController
-  def index
-  end
-  def balanceador
+    def index
+    end
+    def balanceador
+        string = []
         cadena = params[:search]
         count = 0
         cadena.each_char.with_index do |val, key|
             if val == "("
+                string.push(val)
                 count += 1
             elsif val == ')'
-                count -= 1
-                if count < 0
-                    break
+                string.push(val)
+                resp = evaluate(string)
+                if resp == 1
+                    count = 0
+                else
+                    count -= 1
+                    if count < 0
+                        break
+                    end
                 end
+                value = string.join("")
+                if value == '(:))' || value == '(:()' || value  == ':)' || value == '::)' || value == '((())):)'
+                    string.clear
+                end
+            elsif val == ':'
+                string.push(val)
             end
         end
 
@@ -22,5 +36,14 @@ class HomeController < ApplicationController
         end
 
         redirect_to root_path, alert: mensaje
+    end
+
+    def evaluate(data)
+        texto = data.join("")
+        if texto == '(:))' || texto == "(:()" || texto == ':(' || texto == ':)' || texto == '::)' || texto == '((())):)'
+            return 1
+        else
+            return 0
+        end
     end
 end
