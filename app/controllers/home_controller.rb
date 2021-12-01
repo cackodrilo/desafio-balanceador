545b091f743ce2dde6c7499b8bb07a4e5cd257d5
CARLOS <CMATURANA.QUESILLE@GMAIL.COM>
2021-12-01 11:15:16 -0300

    gem pg and rails_12factor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,26 +2,25 @@ class HomeController < ApplicationController
   def index
   end
   def balanceador
-    result = 0
-    cadena = params[:search]
-    count = 0
-    cadena.each_char.with_index do |val, key|
-        if val == "("
-            count += 1
-        elsif val == ')'
-            count -= 1
-            if count < 0
-              break
+        cadena = params[:search]
+        count = 0
+        cadena.each_char.with_index do |val, key|
+            if val == "("
+                count += 1
+            elsif val == ')'
+                count -= 1
+                if count < 0
+                    break
+                end
             end
         end
-    end
 
-    if count == 0
-      mensaje = 'Balanceado'
-    else
-      mensaje = 'Desbalanceado'
-    end
+        if count == 0
+            mensaje = 'Balanceado'
+        else
+            mensaje = 'Desbalanceado'
+        end
 
-    redirect_to root_path, alert: mensaje
-  end
+        redirect_to root_path, alert: mensaje
+    end
 end
