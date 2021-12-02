d97d01c5d2aa4ab7fa9d4a703d667127006559e6
CARLOS <CMATURANA.QUESILLE@GMAIL.COM>
2021-12-02 18:29:41 -0300

    Ac balanceador

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,30 +2,16 @@ class HomeController < ApplicationController
     def index
     end
     def balanceador
-        string = []
         cadena = params[:search]
         count = 0
         cadena.each_char.with_index do |val, key|
             if val == "("
-                string.push(val)
                 count += 1
             elsif val == ')'
-                string.push(val)
-                resp = evaluate(string)
-                if resp == 1
-                    count = 0
-                else
-                    count -= 1
-                    if count < 0
-                        break
-                    end
+                count -= 1
+                if count < 0
+                    break
                 end
-                value = string.join("")
-                if value == '(:))' || value == '(:()' || value  == ':)' || value == '::)' || value == '((())):)'
-                    string.clear
-                end
-            elsif val == ':'
-                string.push(val)
             end
         end
 
@@ -38,12 +24,18 @@ class HomeController < ApplicationController
         redirect_to root_path, alert: mensaje
     end
 
-    def evaluate(data)
-        texto = data.join("")
-        if texto == '(:))' || texto == "(:()" || texto == ':(' || texto == ':)' || texto == '::)' || texto == '((())):)'
-            return 1
-        else
-            return 0
+    ##no se ocupa
+    def order(data)
+        cont = 0
+        data.each_char.with_index do |val, key|
+            if val == "("
+              
+            elsif val == ')'
+            
+            elsif val == ':'
+
+            end
         end
+        return string.join("")
     end
 end
